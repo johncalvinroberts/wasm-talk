4efe85dd9af845bbc1438680d74016ab1b329a7b
John Roberts <john.calvin.roberts@gmail.com>
2020-06-18 22:32:47 +0800

    add trump pix

--- components/Human.js
+++ components/Human.js
@@ -1,29 +1,39 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 const DUMBS = [
   "https://raw.githubusercontent.com/johncalvinroberts/wasm-talk/dev/assets/trump.jpg",
+  "https://raw.githubusercontent.com/johncalvinroberts/wasm-talk/dev/assets/trump2.jpg",
+  "https://raw.githubusercontent.com/johncalvinroberts/wasm-talk/dev/assets/trump3.jpeg",
+  "https://raw.githubusercontent.com/johncalvinroberts/wasm-talk/dev/assets/trump4.jpeg",
+  "https://raw.githubusercontent.com/johncalvinroberts/wasm-talk/dev/assets/trump5.jpg",
+  "https://raw.githubusercontent.com/johncalvinroberts/wasm-talk/dev/assets/trump6.jpg",
 ];
 
 const Human = ({ children }) => {
-  const imgUrl = DUMBS[Math.floor(Math.random() * DUMBS.length)];
+  const imgUrl = useMemo(
+    () => DUMBS[Math.floor(Math.random() * DUMBS.length)],
+    []
+  );
   return (
     <div
       style={{
         display: "flex",
-        justifyContent: "space-between",
+        justifyContent: "flex-start",
         alignItems: "center",
       }}
     >
       <img
         src={imgUrl}
-        style={{ width: "337px", "object-fit": "cover", height: "446px" }}
+        style={{ width: "337px", objectFit: "cover", height: "446px" }}
       ></img>
       <div
         style={{
           fontStyle: "italic",
           borderLeft: "solid 2px black",
-          fontSize: "20px",
+          fontSize: "24px",
           paddingLeft: "4px",
+          marginLeft: "40px",
+          maxWidth: "800px",
         }}
       >
         {children}

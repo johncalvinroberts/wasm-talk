e2b85a00f69e8fdad048f439a679e33b79aa2756
John Roberts <john.calvin.roberts@gmail.com>
2020-06-19 12:09:46 +0800

    feat: update seq diagrams

--- generate-seq-diagram.js
+++ generate-seq-diagram.js
@@ -1,20 +1,29 @@
-const plantuml = require("plantuml");
 const fs = require("fs");
 const { promisify } = require("util");
+const { exec } = require("child_process");
 const path = require("path");
 const readdir = promisify(fs.readdir);
-const readfile = promisify(fs.readFile);
-const writefile = promisify(fs.writeFile);
+
+const renderSvg = (input, output) =>
+  new Promise((resolve, reject) => {
+    try {
+      exec(`seq-cli -i ${input} -o ${output}`, (error, stdout, stderr) => {
+        if (error) reject(error);
+        resolve(stdout);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
 
 (async () => {
   try {
     const files = await readdir("./diagrams");
     for (const file of files) {
-      console.log({ file });
-      const txt = await readfile(`./diagrams/${file}`);
       const { name } = path.parse(file);
-      const svg = await plantuml(txt);
-      await writefile(svg, `./static/${name}.svg`);
+      const input = path.resolve(__dirname, `diagrams/${file}`);
+      const output = path.resolve(__dirname, `assets/${name}.svg`);
+      await renderSvg(input, output);
     }
   } catch (error) {
     console.log({ error });
